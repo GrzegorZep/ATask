1d6b7afcdbf7fd4bae544101744f09beb5a6894f
grzegorz.zep <grzegorz.zep@gmail.com>
2019-10-23 13:50:49 +0200

    result | main method body

--- src/de/atask/Main.java
+++ src/de/atask/Main.java
@@ -1,8 +1,16 @@
 package de.atask;
 
+import de.atask.parcel.Barcode;
+import de.atask.parcel.CheckDigit;
+import de.atask.parcel.Parcel;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Barcode barcode = new Barcode("123");
+
+        CheckDigit result = Parcel.calculate(barcode);
+
+        System.out.println(result.getValue());
     }
 }
--- src/de/atask/parcel/Parcel.java
+++ src/de/atask/parcel/Parcel.java
@@ -8,12 +8,12 @@ import java.util.stream.Stream;
 public class Parcel {
 
     private static final int MOD = 36;
-    private static final Map<String, Byte> BARCODE_VALUES = Stream.of(new Object[][]{
+    private static final Map<String, Integer> BARCODE_VALUES = Stream.of(new Object[][]{
             {"0", 0}, {"1", 1}, {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9},
             {"A", 10}, {"B", 11}, {"C", 12}, {"D", 13}, {"E", 14}, {"F", 15}, {"G", 16}, {"H", 17}, {"I", 18},
             {"J", 19}, {"K", 20}, {"L", 21}, {"M", 22}, {"N", 23}, {"O", 24}, {"P", 25}, {"Q", 26}, {"R", 27},
             {"S", 28}, {"T", 29}, {"U", 30}, {"V", 31}, {"W", 32}, {"X", 33}, {"Y", 34}, {"Z", 35}
-    }).collect(Collectors.toMap(o -> (String) o[0], o -> (Byte) o[1]));
+    }).collect(Collectors.toMap(o -> (String) o[0], o -> (Integer) o[1]));
 
     public static CheckDigit calculate(Barcode barcode) {
 
@@ -21,7 +21,7 @@ public class Parcel {
         cd = MOD;
         Iterator it = barcode.getCharacterList().iterator();
         while (it.hasNext()) {
-            val = BARCODE_VALUES.get(it.next());
+            val = BARCODE_VALUES.get(it.next().toString()).intValue();
             cd = cd + val;
             if (cd > MOD)
                 cd = cd - MOD;
